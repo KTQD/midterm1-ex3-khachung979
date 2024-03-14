49efd79e8f410aee12b7611c0112ca69d159e654
khachung979 <157661539+khachung979@users.noreply.github.com>
2024-03-14 08:19:39 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,14 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) {
-        String inputFile = "input.txt"; // Thay đổi đường dẫn đến input.txt
+        String inputFilePath = "input.txt"; // Thay đổi đường dẫn đến input.txt
         String outputFile = "output.txt";
 
         replaceText(inputFilePath, outputFile, "Nha Trang", "Vũng Tàu");
     }
 
-    public static void replaceText(String inputFile, String outputFileName, String targetWord, String replacement) {
-        try (BufferedReader reader = new BufferedReader(new FileReader(inputFile));
+    public static void replaceText(String inputFilePath, String outputFileName, String targetWord, String replacement) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(inputFilePath));
              BufferedWriter writer = new BufferedWriter(new FileWriter(outputFileName))) {
             String line;
             while ((line = reader.readLine()) != null) {
@@ -19,7 +19,7 @@ public class Main {
             }
             System.out.println("Thay thế thành công và ghi ra file " + outputFileName);
         } catch (IOException e) {
-            System.out.println("Lỗi: " + e.getMessage());
+            System.err.println("Lỗi: " + e.getMessage());
         }
     }
 }
